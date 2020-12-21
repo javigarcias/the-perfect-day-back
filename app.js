6cb1ff5b1f682228731d93adfc288109aae2bcdc
javigarcia <javigarciaweb@gmail.com>
2020-12-21 12:56:01 +0100

    Add Cors Middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const usersRouter = require('./routes/users');
 const commercesRouter = require('./routes/commerces');
+const cors=require('./middleware/cors');
 const app = express();
 const PORT = 3000;
 
 
 app.use(express.json());
+app.use(cors);
 
 app.use('/users', usersRouter);
 app.use('/commerces', commercesRouter);
